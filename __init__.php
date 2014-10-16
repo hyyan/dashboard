b756f38417fa8eb454b1da9db6b9a9e89f1cf8c4
Hyyan Abo Fakher <Tiribthea4hyyan@gmail.com>
2014-10-15 18:56:18 -0700

    Added plugin bootstrap file

---
+++ __init__.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * Plugin Name: Hyyan dashboard
+ * Plugin URI: https://github.com/hyyan/dashboard/
+ * Description: Wordpress plugin to let themes customize the dashboard in their own way
+ * Author: Hyyan Abo Fakher
+ * Version: 0.1
+ * Author URI: https://github.com/hyyan
+ * GitHub Plugin URI: hyyan/dashboard
+ * Domain Path: /languages
+ * Text Domain: dashboard
+ * License: MIT License
+ */
